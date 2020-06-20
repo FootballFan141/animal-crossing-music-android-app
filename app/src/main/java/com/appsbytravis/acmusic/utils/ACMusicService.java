4810dd13c65d3661033014739984d89354e5e1ea
Travis <travisj@appsbytravis.com>
2020-06-19 23:24:25 -0400

    Organized some code. Trying a fix for the NullPointerException in ACMusicService.java for canceling the AlarmManager and for IllegalStateException (Not allowed to start service Intent) in NewHorizons.java, possibly the other game screens had this issue. Update dependencies.
    Signed-off-by: Travis Jones <travisj@appsbytravis.com>

--- app/src/main/java/com/appsbytravis/acmusic/utils/ACMusicService.java
+++ app/src/main/java/com/appsbytravis/acmusic/utils/ACMusicService.java
@@ -3,15 +3,23 @@ package com.appsbytravis.acmusic.utils;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
+
+import java.util.Calendar;
+
+import static com.appsbytravis.acmusic.utils.Constants.CHANGE_MUSIC_REQUESTCODE;
+import static com.appsbytravis.acmusic.utils.Constants.FADE_MUSIC_REQUESTCODE;
 
 public class ACMusicService extends Service {
 
-    private PendingIntent pendingIntent;
+    private Intent changeMusicIntent;
+    private Intent fadeMusicIntent;
     private AlarmManager alarmManager;
-    private PendingIntent pendingIntentFadeMusic;
+    private String assetsPath;
+    private PendingIntent changeMusicPendingIntent;
+    private PendingIntent fadeMusicPendingIntent;
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,57 +28,116 @@ public class ACMusicService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        pendingIntent = intent.getParcelableExtra("pendingIntent");
-        pendingIntentFadeMusic = intent.getParcelableExtra("pendingIntentFadeMusic");
-        alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-
+        changeMusicIntent = intent.getParcelableExtra("changeMusicIntent");
+        fadeMusicIntent = intent.getParcelableExtra("fadeMusicIntent");
+        changeMusicPendingIntent = intent.getParcelableExtra("changeMusicPendingIntent");
+        fadeMusicPendingIntent = intent.getParcelableExtra("fadeMusicPendingIntent");
+        assetsPath = intent.getStringExtra("assetsPath");
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        changeMusicAlarm();
+        fadeMusicAlarm();
         return START_REDELIVER_INTENT;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (pendingIntent != null) {
-            alarmManager.cancel(pendingIntent);
-            pendingIntent.cancel();
+        if (changeMusicPendingIntent != null) {
+            alarmManager.cancel(changeMusicPendingIntent);
+            changeMusicPendingIntent.cancel();
         }
-        if (pendingIntentFadeMusic != null) {
-            alarmManager.cancel(pendingIntentFadeMusic);
-            pendingIntentFadeMusic.cancel();
+        if (fadeMusicPendingIntent != null) {
+            alarmManager.cancel(fadeMusicPendingIntent);
+            fadeMusicPendingIntent.cancel();
         }
         alarmManager = null;
-        pendingIntent = null;
+        changeMusicPendingIntent = null;
+        fadeMusicPendingIntent = null;
     }
 
     @Override
     public boolean stopService(Intent name) {
         super.stopService(name);
-        if (pendingIntent != null) {
-            alarmManager.cancel(pendingIntent);
-            pendingIntent.cancel();
+        if (changeMusicPendingIntent != null) {
+            alarmManager.cancel(changeMusicPendingIntent);
+            changeMusicPendingIntent.cancel();
         }
-        if (pendingIntentFadeMusic != null) {
-            alarmManager.cancel(pendingIntentFadeMusic);
-            pendingIntentFadeMusic.cancel();
+        if (fadeMusicPendingIntent != null) {
+            alarmManager.cancel(fadeMusicPendingIntent);
+            fadeMusicPendingIntent.cancel();
         }
         alarmManager = null;
-        pendingIntent = null;
+        changeMusicPendingIntent = null;
+        fadeMusicPendingIntent = null;
         return true;
     }
 
     @Override
     public void onTaskRemoved(Intent rootIntent) {
         super.onTaskRemoved(rootIntent);
-        if (pendingIntent != null) {
-            alarmManager.cancel(pendingIntent);
-            pendingIntent.cancel();
+        if (changeMusicPendingIntent != null) {
+            alarmManager.cancel(changeMusicPendingIntent);
+            changeMusicPendingIntent.cancel();
         }
-        if (pendingIntentFadeMusic != null) {
-            alarmManager.cancel(pendingIntentFadeMusic);
-            pendingIntentFadeMusic.cancel();
+        if (fadeMusicPendingIntent != null) {
+            alarmManager.cancel(fadeMusicPendingIntent);
+            fadeMusicPendingIntent.cancel();
         }
         alarmManager = null;
-        pendingIntent = null;
+        changeMusicPendingIntent = null;
+        fadeMusicPendingIntent = null;
         stopSelf();
     }
+
+    private void changeMusicAlarm() {
+        Calendar calendar = setCalendar();
+        calendar.add(Calendar.HOUR_OF_DAY, 1);
+        long timeInMillis = calendar.getTimeInMillis();
+        PendingIntent changeMusicIntent = PendingIntent.getBroadcast(getBaseContext(), CHANGE_MUSIC_REQUESTCODE, this.changeMusicIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManager.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timeInMillis, changeMusicIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, timeInMillis, changeMusicIntent);
+        } else {
+            alarmManager.set(AlarmManager.RTC_WAKEUP, timeInMillis, changeMusicIntent);
+        }
+    }
+
+    private void fadeMusicAlarm() {
+
+        Calendar calendarFadeMusic = getCalendar();
+        calendarFadeMusic.set(Calendar.HOUR_OF_DAY, calendarFadeMusic.get(Calendar.HOUR_OF_DAY));
+        calendarFadeMusic.set(Calendar.MINUTE, 59);
+        calendarFadeMusic.set(Calendar.SECOND, 55);
+        calendarFadeMusic.set(Calendar.MILLISECOND, 0);
+        long timeInMillisFadeMusic = calendarFadeMusic.getTimeInMillis();
+
+        PendingIntent fadeMusicIntent = PendingIntent.getBroadcast(getBaseContext(), FADE_MUSIC_REQUESTCODE, this.fadeMusicIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        AlarmManager alarmManagerFadeMusic = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManagerFadeMusic.cancel(fadeMusicIntent);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            alarmManagerFadeMusic.setExactAndAllowWhileIdle(AlarmManager.RTC_WAKEUP, timeInMillisFadeMusic, fadeMusicIntent);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManagerFadeMusic.setExact(AlarmManager.RTC_WAKEUP, timeInMillisFadeMusic, fadeMusicIntent);
+        } else {
+            alarmManagerFadeMusic.set(AlarmManager.RTC_WAKEUP, timeInMillisFadeMusic, fadeMusicIntent);
+        }
+    }
+
+    private Calendar setCalendar() {
+        Calendar calendar = getCalendar();
+
+        calendar.set(calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH),
+                calendar.get(Calendar.HOUR_OF_DAY), 0, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
+    }
+
+    private Calendar getCalendar() {
+        return Calendar.getInstance();
+    }
+
 }
